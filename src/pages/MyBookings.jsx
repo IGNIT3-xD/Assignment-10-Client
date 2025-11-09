5ad083e34f4c049233f032cd25f47401cbb97091
IGNIT3-xD <md.imranali089@gmail.com>
2025-11-09 23:47:58 +0600

    Cancel booking functionality added

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useAxios } from '../hooks/useAxios';
 import { use } from 'react';
 import { AuthContext } from './../contexts/AuthContext';
+import Swal from 'sweetalert2';
 
 const MyBookings = () => {
     const [data, setData] = useState([])
@@ -17,6 +18,36 @@ const MyBookings = () => {
             })
     }, [instance, user])
 
+    const handleCancel = (id) => {
+        // console.log(id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "oklch(70.9% 0.169 91.605)",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                instance.delete(`/booking/${id}`)
+                    .then(res => {
+                        // console.log(res.data);
+                        if (res.data.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your booking has been canceled.",
+                                icon: "success"
+                            });
+
+                            const remainingData = data.filter(service => service._id !== id)
+                            setData(remainingData)
+                        }
+                    })
+            }
+        });
+    }
+
     if (loading) {
         return <p className='my-20 text-3xl font-bold text-center'>Loading...</p>
     }
@@ -26,8 +57,8 @@ const MyBookings = () => {
             <h1 className='font-bold text-2xl md:text-3xl text-center my-6'>My <span className='text-amber-300'> Bookings </span> <span className='text-xs'>({data.length})</span> </h1>
             {
                 data.length === 0 ? <p className='text-2xl my-10 text-center font-bold'>Currently you don't have any <span className='text-amber-300'>booking</span></p> :
-                    <div className='my-10 overflow-x-auto'>
-                        <table className="table table-xs lg:table-md border border-black/10 bg-white">
+                    <div className='my-20 overflow-x-auto'>
+                        <table className="table border border-black/10 bg-white">
                             <thead>
                                 <tr>
                                     <th>SL No.</th>
@@ -52,7 +83,7 @@ const MyBookings = () => {
                                             <td className='font-bold'>${service.price}</td>
                                             <td>{service.date}</td>
                                             <td>
-                                                <button className='btn btn-xs text-green-600 outline outline-amber-500'>Cancel</button>
+                                                <button onClick={() => handleCancel(service._id)} className='btn btn-xs text-green-600 outline outline-amber-500'>Cancel</button>
                                             </td>
                                         </tr>
                                     )
